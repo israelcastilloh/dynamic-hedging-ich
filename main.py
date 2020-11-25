fe377f65d28945b71d49bf0788ec28e37392dc80
israelcastilloh <castillo.israelh@gmail.com>
2020-11-25 14:48:39 -0600

    update

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from data import *
 from functions import *
 
 escenario_historicos = precios_escenarios(futuros_hist_daily_modelo, futuros_hist_daily)
-print(escenario_historicos)
+#print(escenario_historicos)
 
 coberturas_historicas = query_opciones(escenario_historicos)
-#print(coberturas_historicas)
+print(coberturas_historicas)
